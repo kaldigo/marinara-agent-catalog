5c0ca5fde33995f061fbd7d8b187ff99f3bd3fc3
Kaldigo <6437691+kaldigo@users.noreply.github.com>
2026-08-22 12:41:32 +0100

    chore: archive retired package sources

--- response-keeper/src/client/constants.js
+++
@@ -1,6 +0,0 @@
-export const PACKAGE_ID = "response-keeper";
-export const PACKAGE_NAME = "Response Keeper";
-export const PACKAGE_VERSION = "1.0.1";
-export const PUBLIC_API_KEY = "__marinaraResponseKeeper";
-export const RUNTIME_KEY = "__marinaraResponseKeeperRuntime";
-export const EXTRA_KEY = "responseKeeper";
